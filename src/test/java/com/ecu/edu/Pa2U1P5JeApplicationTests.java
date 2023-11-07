2e90709cc5eaea180f274d002616eeb2ce75c028
MrEvolt15 <joelespinosa1a@hotmail.com>
2023-11-07 18:57:38 -0500

    Mi primer commit

---
+++ src/test/java/com/ecu/edu/Pa2U1P5JeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecu.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U1P5JeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
