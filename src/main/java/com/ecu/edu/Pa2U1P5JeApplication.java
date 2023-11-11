0198856bfcf73ede61ed9d230e28fd3b66d8ed52
MrEvolt15 <joelespinosa1a@hotmail.com>
2023-11-10 19:08:10 -0500

    Taller 4

--- src/main/java/com/ecu/edu/Pa2U1P5JeApplication.java
+++ src/main/java/com/ecu/edu/Pa2U1P5JeApplication.java
@@ -17,6 +17,7 @@ public class Pa2U1P5JeApplication implements CommandLineRunner{
 		System.out.println("Hola Mundo !!");
 		System.out.println("Soy Joel Espinosa");
 		System.out.println("cambio en la misma rama");
+		System.out.println("taller 4");
 	}
 
 }
